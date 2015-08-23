3f135ef001ad02dcb907ce51db8e1a49fd59b377
Yojim6o <jgs2017@gmail.com>
2015-08-23 15:48:55 -0500

    Make bug lanes and bug moves/speeds

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // contants
 var PLAYER_X_COORDINATE = 200;
-var PLAYER_Y_COORDINATE = 400;
+var PLAYER_Y_COORDINATE = 390;
 
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
@@ -20,7 +20,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-
+    if (this.x < 525) {
+        this.x += this.speed * dt;  
+    } else {
+        this.x = -100;
+        this.y = lanes[getRandomInt(0,3)];
+        this.speed = getRandomInt(6,11)*50;
+    };
 };
 
 // Draw the enemy on the screen, required method for game
@@ -28,7 +34,7 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-
+var lanes = [60,140,225];
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -53,8 +59,18 @@ Player.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var bug1 = new Enemy(-100,50,1);
-var allEnemies = [bug1];
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+
+var bug1 = new Enemy(100,lanes[getRandomInt(0,3)],300);
+var bug2 = new Enemy(100,lanes[getRandomInt(0,3)],300);
+var bug3 = new Enemy(100,lanes[getRandomInt(0,3)],300);
+var bug4 = new Enemy(100,lanes[getRandomInt(0,3)],300);
+var bug5 = new Enemy(100,lanes[getRandomInt(0,3)],300);
+var allEnemies = [bug1,bug2,bug3,bug4,bug5];
+bug1.update();
 var player = new Player(PLAYER_X_COORDINATE,PLAYER_Y_COORDINATE);
 
 
